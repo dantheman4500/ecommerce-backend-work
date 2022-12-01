4b62f049d056905fc7fe1f1c6647bd98da343b7a
dantheman4500 <danielromero4500@gmail.com>
2022-11-30 19:51:22 -0800

    commit

---
+++ models/ProductTag.js
@@ -0,0 +1,46 @@
+//* import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+
+//* import our database connection from config.js
+const sequelize = require('../config/connection');
+
+//* Initialize Product model (table) by extending off Sequelize's Model class
+class ProductTag extends Model {}
+
+//* set up fields and rules for Product model
+ProductTag.init(
+    {
+        //* define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        //* define product_id column
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "product",
+                key: "id"
+            }
+        },
+        //* define tag_id column
+        tag_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "tag",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag',
+    }
+);
+
+module.exports = ProductTag;
---
+++ models/Tag.js
@@ -0,0 +1,34 @@
+//* import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+
+//* import our database connection from config.js
+const sequelize = require('../config/connection.js');
+
+//* import our database connection from config.js
+class Tag extends Model {}
+
+//* set up fields and rules for Product model
+Tag.init(
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define tag_name column
+        tag_name: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag',
+    }
+);
+
+module.exports = Tag;
